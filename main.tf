ac1244de4a739f5f157e43d536d21b9322f5816f
Gagandeep Singh <admin.gagan@gmail.com>
2022-06-07 01:23:59 +0530

    empty the file

--- main.tf
+++ main.tf
@@ -1,17 +1,1 @@
-provider "aws" {
-  region = "us-east-2"
-}
 
-variable "hw" {
-type = string
-default = "t2.nano"
-}
-
-resource "aws_instance" "myawsserver1" {
-  ami = "ami-0603cbe34fd08cb81"
-  instance_type = var.hw
-  tags = {
-    Name = "gagan-vm1-change2"
-    Env = "Test"
-  }
-}
