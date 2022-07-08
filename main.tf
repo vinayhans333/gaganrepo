cda8a738830ad56fef7b9b91769c047ab18aed6b
Gagandeep Singh <admin.gagan@gmail.com>
2022-07-08 13:15:25 +0530

    updated owner

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "myawsserver" {
   tags = {
     Name = lower (var.name)
     env = upper (var.env)
-    owner = upper ("Gagandeep")
+    owner = upper ("Gagandeep Singh")
     dept = "Training"
     client = "jpmc"
   }
