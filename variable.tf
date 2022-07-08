7a2c40de2f412ae096606e628b1b44d552e2ab25
Gagandeep Singh <admin.gagan@gmail.com>
2022-07-08 12:27:29 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "image" {
+  type = string
+  default = "ami-02f3416038bdb17fb"
+}
+
+variable "hw" {
+  default = "t2.micro"
+}
+
+variable "name" {
+  default = "gagan-manual-tf"
+}
+
+variable "env" {
+  default = "prod"
+}
+
+variable "bucket-name" {
+  default = "gagandefaultbucket"
+}
