8971a656850654bcabe2b15043ef3fdf1670613b
Gagandeep Singh <admin.gagan@gmail.com>
2022-09-23 11:46:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "myawsserver" {
   tags = {
     Name = lower (var.name)
     env = upper (var.env)
-    owner = upper ("Gagandeep Singh")
+    owner = upper ("Techlanders")
     dept = "lab"
     client = "hexaware"
   }
