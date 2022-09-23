5854e69ece838e0544b6e833eb8c7f2c7d41058c
vinayhans333 <52851342+vinayhans333@users.noreply.github.com>
2022-09-23 12:11:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "myawsserver" {
   ami = var.image
-  instance_type = "t2.nano"
+  instance_type = "t2.micro"
  # key_name = "gagan-new"
   tags = {
     Name = lower (var.name)
